8b93ced45e9d59a2e0dcb924714e031751ccfbe3
David Francos <opensource@davidfrancos.net>
2020-04-18 16:59:58 +0200

    Added extension filtering

--- src/mixins/torrents.js
+++ src/mixins/torrents.js
@@ -1,3 +1,9 @@
+const EBOOK_EXTENSIONS = [
+    "epub", "mobi", "pdf", "lrf", "cbr", "cbz", "cb7", "cbt ", "cba ", "chm",
+    "djvu", "doc", "docx", "pdb", "fb2", "ceb", "xeb", "html", "htm", "ibooks",
+    "azw", "azw3", "kf8", "kfx", "lit", "prc", "opf", "pdg ", "ps", "xps", "lrx"
+];
+
 export default {
     methods: {
         getTorrentFiles: (client, torrent_origin) => {
@@ -6,10 +12,10 @@ export default {
             const files = [];
             client.add(torrent_origin.torrent, torrent => {
                 for (let file of torrent.files) {
-                    files.push({
-                        file: file, 
-                        label_name: file.path
-                    });
+                    let ext = file.path.split(".").pop().toLowerCase();
+                    if (EBOOK_EXTENSIONS.indexOf(ext) != -1) {
+                        files.push({file: file, label_name: file.path});
+                    }
                 }
                 resolve_func(files);
             });

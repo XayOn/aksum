d90857a252ba07b30fc679fbeb877b9a26435779
David Francos <opensource@davidfrancos.net>
2020-04-20 01:48:19 +0200

    Parse book filename and show only base filename and extension

--- src/mixins/torrents.js
+++ src/mixins/torrents.js
@@ -1,4 +1,5 @@
 import magnet from 'magnet-uri';
+import parsePath from 'parse-filepath';
 
 const EBOOK_EXTENSIONS = [
     "epub", "mobi", "pdf", "lrf", "cbr", "cbz", "cb7", "cbt ", "cba ", "chm",
@@ -13,16 +14,14 @@ export default {
                 ? JSON.parse(localStorage.torrent_list)
                 : [];
         },
-        looksLikeBook: function (path) {
-            let ext = path.split(".").pop().toLowerCase();
-            return EBOOK_EXTENSIONS.indexOf(ext) != -1
-        },
         getTorrentFiles: function (client, torrentOrigin, torrentList) {
             let parsed = magnet.encode(torrentOrigin.decoded);
             client.add(parsed, torrent => {
                 for (let file of torrent.files) {
-                    if (this.looksLikeBook(file.path)) {
-                        torrentList.push({torrent: torrentOrigin.decoded.infoHash, file: file, label_name: file.path});
+                    let parsedPath = parsePath(file.path);
+                    let ext = parsedPath.ext.substring(1);
+                    if (EBOOK_EXTENSIONS.indexOf(ext) != -1) {
+                        torrentList.push({torrent: torrentOrigin.decoded.infoHash, file: file, label_name: `[${ext}] ${parsedPath.name}`});
                     }
                 }
             });

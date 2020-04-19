8b6850614bee25f8767462eab9805f4adfa5b2ef
David Francos <opensource@davidfrancos.net>
2020-04-19 02:35:17 +0200

    Added setting dialog to import from gist

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -4,11 +4,22 @@ Append source
 <template>
   <v-container>
     <h2 class="text-center">Edit book sources (Torrent links)</h2>
+
+    <v-text-field
+      v-model="gist_uri"
+      @keydown.enter="AddSourceGist"
+      @click:append-outer="AddSourceGist"
+      append-outer-icon="mdi-plus"
+      placeholder="Github GIST url containing one magnet per line"
+      single-line
+    ></v-text-field>
+
     <v-text-field
       v-model="torrent_uri"
       @keydown.enter="AddSource"
       @click:append-outer="AddSource"
       append-outer-icon="mdi-plus"
+      placeholder="Magnet link"
       :loading="loading"
       single-line
     ></v-text-field>
@@ -37,8 +48,11 @@ export default {
   data: function() {
     return {
       display: false,
+      gist_uri: "",
       torrent_uri: "",
-      torrent_list: JSON.parse(localStorage.torrent_list?localStorage.torrent_list:"[]"),
+      torrent_list: JSON.parse(
+        localStorage.torrent_list ? localStorage.torrent_list : "[]"
+      ),
       headers: [
         { text: "Name", align: "start", value: "decoded.name" },
         { text: "Hash", align: "start", value: "decoded.infoHash" }
@@ -48,12 +62,18 @@ export default {
     };
   },
   methods: {
+    AddSourceGist: function() {
+      let split = this.gist_uri.split("/");
+      let searchParams = new URLSearchParams(window.location.search);
+      searchParams.set("gist", `${split[3]}_${split[4]}_${split[6]}`);
+      window.location.search = searchParams.toString();
+    },
     AddSource: async function() {
       this.loading = true;
       let added_torrent = this.addTorrent(this.torrent_list, this.torrent_uri);
       this.torrent_list = [...this.torrent_list, added_torrent];
       this.$emit("torrentAdded", added_torrent);
-        this.torrent_uri = ""
+      this.torrent_uri = "";
       this.loading = false;
     },
     deleteSources: function() {
--- src/mixins/gist.js
+++ src/mixins/gist.js
@@ -6,7 +6,7 @@ export default {
             let url = `https://yacdn.org/proxy/https://gist.githubusercontent.com/${gist_params[0]}/${gist_params[1]}/raw/${gist_params[2]}`;
             console.log(url);
             let response = await axios.get(url)
-            return response.data.split(";")
+            return response.data.split("\n")
         }
     }
 }
